ad656acbc47d41833faae939b4b521ed9c4863ed
coocooder1 <elvs97121041@gmail.com>
2024-07-29 17:54:34 +0800

    complete the sign in page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,68 @@
 
-import React from 'react'
 
-export default function Signin() {
+import React,{useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({...formData,[e.target.id]:e.target.value})
+
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+      try {
+        setLoading(true)
+        setError(false);
+        const res = await fetch('/api/auth/signin', {
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+          const data = await res.json();
+
+          setLoading(false)
+          if (data.success === false) {
+            setError(true);
+            return
+          }
+          navigate('/')
+        
+           
+           
+        
+      } catch (error) {
+        setLoading(false);
+        setError(true)
+        
+      }
+  };
+
+
   return (
-    <div>Signin</div>
+    <div className='p-3 max-w-lg mx-auto' >
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign in</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+       
+        <input type='email' placeholder='email' id='email' onChange={handleChange}
+        className='bg-slate-100 rounded-lg p-3'/>
+        <input type='password' placeholder='password' id='password' onChange={handleChange}
+        className='bg-slate-100 rounded-lg p-3'/>
+        <button disabled={loading} className='bg-slate-500 rounded-lg text-white p-3 uppercase 
+        hover:opacity-75'>{loading ? 'Loading...' : 'Sign In'}</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Don't Have an account?</p>
+        <Link to='/sign-up'>
+        <span className='text-blue-500'>Sign up</span>
+        </Link>
+      </div>
+      <p className='text-red-500 mt-5'>{error && 'Something went wrong!'}</p>
+    </div>
   )
 }
+
