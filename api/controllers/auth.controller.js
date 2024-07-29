0b9b87b4ecc2d80516cd68ec13d0e69b5c42a2fb
coocooder1 <elvs97121041@gmail.com>
2024-07-29 17:34:00 +0800

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10)
@@ -17,3 +18,20 @@ export const signup = async (req, res, next) => {
     }
    
 };
+
+export const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+    try{
+        const validUser = await User.findOne({ email});
+        if (!validUser) return next(errorHandler(404, '找不到用戶'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, '數據錯誤'));
+        const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET );
+        const {password: hashedPassword, ...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000);
+        res.cookie('access_token', token, { httpOnly: true, expires:expiryDate}).status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
+    
+}
